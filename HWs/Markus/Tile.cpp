296d2b40a70b63e16c6117eb8d12925fb8a9f115
maxipi <markus@tug-swl-b08.tu-graz.ac.at>
2015-03-27 11:56:20 +0100

    Hausübung

---
+++ HWs/Markus/Tile.cpp
@@ -0,0 +1,19 @@
+//------------------------------------------------------------------------------
+// Tile.cpp
+//
+// Group: Group 9, study assistant Philip Loibl
+//
+// Authors: Markus Pichler 1331070
+// 
+//------------------------------------------------------------------------------
+//
+
+#include <iostream>
+#include "Tile.h"
+
+//------------------------------------------------------------------------------
+Tile::Tile(Type side, Color topcolor)
+{
+  side_ = side;
+  topcolor_ = topcolor;
+}
