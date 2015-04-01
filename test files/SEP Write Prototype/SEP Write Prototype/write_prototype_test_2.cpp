810e9b12e6fc94e80ba81670702054c4eef97a56
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2015-04-01 09:54:39 +0200

    Fileheader and first line of board

--- test files/SEP Write Prototype/SEP Write Prototype/write_prototype_test_2.cpp
+++ test files/SEP Write Prototype/SEP Write Prototype/write_prototype_test_2.cpp
@@ -8,41 +8,51 @@ int main()
 {
   typedef struct
   {
-    uint32_t signature; // char[4] // TRAX // hex: 54 52 41 58 // little endian: 58 41 52 54
-    uint8_t active_player; // char // white (1) or red (2)
-    int8_t minX; // signed char
-    int8_t minY; // signed char
-    int8_t maxX; // signed char
-    int8_t maxY; // signed char
+    char *signature; // char[4] // TRAX // hex: 54 52 41 58 // little endian: 58 41 52 54
+    char active_player; // char // white (1) or red (2)
+    signed char minX; // signed char
+    signed char minY; // signed char
+    signed char maxX; // signed char
+    signed char maxY; // signed char
   } BoardHeader; // signed char
 
   typedef struct
   {
-    uint8_t side; // char // no figure (0), cross (1), Kurve1 "/" (2), Kurve2 "\" (3)
-    uint8_t top_color; // char // no figure (0), white end (1), red end (2)
+    char side; // char // no figure (0), cross (1), Kurve1 "/" (2), Kurve2 "\" (3) // offset 0
+    char top_color; // char // no figure (0), white end (1), red end (2) // offset 1
   } BoardTiles;
 
+  // Write values into header
   BoardHeader FileHeader;
-  FileHeader.signature = TYPE;
-  
+  //FileHeader.signature = { 'T', 'R', 'A', 'X', '\0' };
+  FileHeader.signature = "TRAX";
+  FileHeader.active_player = 1;
+  FileHeader.minX = 0;
+  FileHeader.minY = 0;
+  FileHeader.maxX = 10;
+  FileHeader.maxY = 20;
 
+  // Write header into file
   std::ofstream file("test", std::ios::out | std::ios::trunc | std::ios::binary);
-  //file << FileHeader.signature;
+  
+  file << FileHeader.signature;
+  file << FileHeader.active_player;
+  file << FileHeader.minX;
+  file << FileHeader.minY;
+  file << FileHeader.maxX;
+  file << FileHeader.maxY;
 
-  char sig[5] = "TRAX";
-  file << sig;
-  char a_p = 1;
-  file << a_p;
-  char min_x = 0;
-  file << min_x;
-  char min_y = 0;
-  file << min_y;
-  char max_x = 10;
-  file << max_x;
-  char max_y = 10;
-  file << max_y;
-
-
+  // Write each line of the board from (minX,minY) to (maxX,maxY) from left to right
+  // PSEUDO
+  char num_1 = 1;
+  char num_2 = 0;
+  long pos = file.tellp();
+  // Sets position to beginning of file
+  pos = 0;
+  // Sets position to next line in binary file
+  file.seekp(pos+16);
+  file << num_1;
+  file << num_2;
 
   return 0;
 }
