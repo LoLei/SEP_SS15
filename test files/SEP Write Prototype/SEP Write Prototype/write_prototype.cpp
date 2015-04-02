7104a5b8ee2814948d2ee199d3930793f5ea5f9f
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2015-04-02 14:51:00 +0200

    Add command line argument functionality for file name

--- test files/SEP Write Prototype/SEP Write Prototype/write_prototype.cpp
+++ test files/SEP Write Prototype/SEP Write Prototype/write_prototype.cpp
@@ -1,29 +1,60 @@
-// Version 2.1.1
+// Version 2.2
 // TODO Swapping active player only happens when next player makes a turn
 // not when a forced tile is placed
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
 #define TYPE 1480675924 // TRAX in dec from hex in little endian
-#define AP_OFFSET 4
-#define AP_WHITE 1
-#define AP_RED 2
-#define NO_FIGURE 0
-#define CROSS 1
-#define CURVE_1 2
-#define CURVE_2 3
-#define TOP_WHITE 1
-#define TOP_RED 2
+#define AP_OFFSET     4
+#define AP_WHITE      1
+#define AP_RED        2
+#define NO_FIGURE     0
+#define SIDE_CROSS    1
+#define SIDE_CURVE_1  2
+#define SIDE_CURVE_2  3
+#define TOP_WHITE     1
+#define TOP_RED       2
 
-int main()
+void writeFile(std::string file_name);
+
+//------------------------------------------------------------------------------
+// Main function - used to call write function and pass arguments
+//
+// @param argc the amount of arguments passed to the programm
+// @param argv list of arguments
+//        first one can be -g, makes write be called at each AddTile
+//        second one is file name for the output binary file
+//
+// @return int for success
+//
+int main(int argc, char *argv[])
+{
+  std::string file_name;
+  // File name is either from initial program start or from sep> command
+  if (argc == 3)
+  {
+    file_name = argv[2];
+    std::cout << file_name << std::endl;
+  }
+  writeFile(file_name);
+  return 0;
+}
+
+//------------------------------------------------------------------------------
+// Write function - used to write the actual binary file
+//
+// @param file_name the name the user wants the output file to be called
+//
+void writeFile(std::string file_name)
 {
   long pos;
 
   typedef struct
   {
-    char *signature; // char[4] // TRAX // hex: 54 52 41 58 // little endian: 58 41 52 54 // Offset 0
-    char active_player; // char // white (1) or red (2) // Offset 4
+    char *signature; // char[4] // TRAX 
+    char active_player; // char // white (1) or red (2)
     signed char minX; // signed char
     signed char minY; // signed char
     signed char maxX; // signed char
@@ -32,13 +63,12 @@ int main()
 
   typedef struct
   {
-    char side; // char // no figure (0), cross (1), Kurve1 "/" (2), Kurve2 "\" (3) // offset 0
-    char top_color; // char // no figure (0), white end (1), red end (2) // offset 1
+    char side; // no figure (0), cross (1), Kurve1 "/" (2), Kurve2 "\" (3)
+    char top_color; // no figure (0), white end (1), red end (2) // offset 1
   } BoardTiles;
 
   // Write values into header
   FileHeader file_header;
-  //file_header.signature = { 'T', 'R', 'A', 'X', '\0' };
   file_header.signature = "TRAX";
   file_header.active_player = AP_WHITE;
   file_header.minX = 0;
@@ -46,9 +76,12 @@ int main()
   file_header.maxX = 1;
   file_header.maxY = 1;
 
-  // Write header into file
-  std::ofstream file("trax2", std::ios::out | std::ios::trunc | std::ios::binary);
   
+  // Open file for writing
+  std::ofstream file(file_name,
+    std::ios::out | std::ios::trunc | std::ios::binary);
+
+  // Write header into file
   file << file_header.signature;
   file << file_header.active_player;
   file << file_header.minX;
@@ -56,14 +89,14 @@ int main()
   file << file_header.maxX;
   file << file_header.maxY;
 
-  // Write each line of the board from (minX,minY) to (maxX,maxY) from left to right
+  // Write each line of the board from (minX,minY) to (maxX,maxY)
+  // from left to right
   // Write tiles into file
-  // TODO Change active_player each turn
   BoardTiles board_tiles;
   //--------------------------------------------------------------------------//
   // Active player = White, set already
-  board_tiles.side = 1;
-  board_tiles.top_color = 2;
+  board_tiles.side = SIDE_CROSS;
+  board_tiles.top_color = TOP_RED;
 
   file << board_tiles.side;
   file << board_tiles.top_color;
@@ -78,9 +111,9 @@ int main()
   // Sets position to where it left off
   file.seekp(pos);
 
-  board_tiles.side = CROSS;
+  board_tiles.side = SIDE_CROSS;
   board_tiles.top_color = TOP_RED;
-  
+
   file << board_tiles.side;
   file << board_tiles.top_color;
   // Save position
@@ -94,9 +127,9 @@ int main()
   // Sets position to where it left off
   file.seekp(pos);
 
-  board_tiles.side = CROSS;
+  board_tiles.side = SIDE_CROSS;
   board_tiles.top_color = TOP_RED;
-  
+
   file << board_tiles.side;
   file << board_tiles.top_color;
   // Save position
@@ -110,13 +143,11 @@ int main()
   // Sets position to where it left off
   file.seekp(pos);
 
-  board_tiles.side = CURVE_2;
+  board_tiles.side = SIDE_CURVE_2;
   board_tiles.top_color = TOP_RED;
 
   file << board_tiles.side;
   file << board_tiles.top_color;
   // Save position
   pos = file.tellp();
-
-  return 0;
 }
