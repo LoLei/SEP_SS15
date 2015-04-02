6f9d61f30b712f91c607cf5fd10428353ce49896
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2015-04-02 11:14:46 +0200

    Additional try except tests

--- test files/SEP Try Except Lo/SEP Try Except Lo/try_except.cpp
+++ test files/SEP Try Except Lo/SEP Try Except Lo/try_except.cpp
@@ -4,18 +4,38 @@ int main()
 {
   try
   {
-    int age_mom = 50;
-    int age_son = 55;
+    float age_mom;
+    float age_kid;
 
-    if (age_son > age_mom)
+    std::cout << "We're going to divide your mom's age by your age" << std::endl;
+    std::cout << "Enter your mom's age and your age" << std::endl << std::endl;
+    std::cin >> age_mom >> age_kid;
+
+    // Kid older than mom
+    if (age_kid > age_mom)
     {
-      throw 99;
+      throw 1;
     }
-    std::cout << "Success" << std::endl;
+    // Kid's age is 0, can't divide by 0
+    if (age_kid == 0)
+    {
+      throw 2;
+    }
+
+    // No exception caught
+    std::cout << "The result is: " << age_mom / age_kid << std::endl;
+
   }
   catch (int x)
   {
-    std::cout << "Son cannot be older than mom. Error code: " << x << std::endl;
+    if (x == 1)
+    {
+      std::cout << "You cannot be older than your mom. Error code: " << x << std::endl;
+    }
+    if (x == 2)
+    {
+      std::cout << "Can't divide by 0. Error Code: " << x << std::endl;
+    }
+    
   }
-  std::cout << "Test" << std::endl;
 }
