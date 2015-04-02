378070018ac686d0fa2d834fe4c99b65d199abef
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2015-04-02 10:08:47 +0200

    Try except experiments

---
+++ test files/SEP Try Except Lo/SEP Try Except Lo/try_except.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+  try
+  {
+    int age_mom = 50;
+    int age_son = 30;
+
+    if (age_son > age_mom)
+    {
+      throw 99;
+    }
+    std::cout << "Success" << std::endl;
+  }
+  catch (int x)
+  {
+    std::cout << "Son cannot be older than mom. Error code: " << x << std::endl;
+  }
+}
